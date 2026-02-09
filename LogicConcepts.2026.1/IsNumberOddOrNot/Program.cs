c87b90233995e801d325ada873346481315db381
teffidev <tefyberal09@gmail.com>
2026-02-09 16:02:49 -0500

    Complete exercise 01

---
+++ LogicConcepts.2026.1/IsNumberOddOrNot/Program.cs
@@ -0,0 +1,28 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.Write("Ingrese número entero o presione 'Salir' para salir: ");
+    numberString = Console.ReadLine();
+    if (numberString!.ToLower() == "salir")
+    {
+        continue;
+    }
+
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El número {numberInt} es Par");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numberInt} es Impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Lo que ingresaste: {numberString} no es un número entero");
+    }    
+} while (numberString!.ToLower() != "salir");
+Console.WriteLine("Game over.");
