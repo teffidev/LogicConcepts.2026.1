cac52c14f7512a1707eafeb2d216554cd81c4f70
teffidev <tefyberal09@gmail.com>
2026-02-16 15:10:11 -0500

    finished exercise 2

---
+++ LogicConcepts.2026.1/TheLargestNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+do
+{
+    Console.WriteLine("Ingrese tres números diferentes.");
+    var a = ConsoleExtensions.GetInt("Ingrese primer número: ");
+    var b = ConsoleExtensions.GetInt("Ingrese segundo número: ");
+    var c = ConsoleExtensions.GetInt("Ingrese tercer número: ");
+
+    if (a > b && a > c)
+    {
+        Console.WriteLine($"El número mayor es: {a}");
+    } else if (b > a && b > c)
+    {
+        Console.WriteLine($"El número mayor es: {b}");
+    } else
+    {
+        Console.WriteLine($"El número mayor es: {c}");
+    }
+
+    do
+    {
+        answer = ConsoleExtensions.GetValidOptions("¿Desea continuar [S]i, [N]o}?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
