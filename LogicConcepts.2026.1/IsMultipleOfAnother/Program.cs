88f2230af6451002fdd9eaf033a7f74c0c169fd0
teffidev <tefyberal09@gmail.com>
2026-02-18 13:42:25 -0500

    Is multiple of another

---
+++ LogicConcepts.2026.1/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,18 @@
+﻿using Shared;
+
+do
+{
+    var a = ConsoleExtensions.GetInt("Ingrese primer número : ");
+    var b = ConsoleExtensions.GetInt("Ingrese segundo número : ");
+
+    if (a % b == 0)
+    {
+        Console.WriteLine($"{a} es múltiplo de {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} no es múltiplo de {b}");
+    }
+
+
+} while (true);
