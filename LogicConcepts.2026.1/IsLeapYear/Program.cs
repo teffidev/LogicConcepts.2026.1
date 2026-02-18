c222d0f6e71a64d199fe71c45e4fcc75d341ca9a
teffidev <tefyberal09@gmail.com>
2026-02-18 14:10:47 -0500

    Is leap year

---
+++ LogicConcepts.2026.1/IsLeapYear/Program.cs
@@ -0,0 +1,48 @@
+﻿using Shared;
+
+do
+{
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = ConsoleExtensions.GetInt("Ingrese año: ");
+
+    if (year == currentYear)
+    {
+        message = "es";
+    }
+    else if (year > currentYear)
+    {
+        message = "será";
+    }
+    else
+    {
+        message = "fue";
+    }
+
+
+    if (year % 4 == 0 )
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                Console.WriteLine($"El año: {year}, SI {message} bisiesto");
+            }
+            else
+            {
+                Console.WriteLine($"El año: {year}, NO {message} bisiesto");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"El año: {year}, SI {message} bisiesto");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El año: {year}, NO {message} bisiesto");
+    }
+
+
+} while (true);
+
