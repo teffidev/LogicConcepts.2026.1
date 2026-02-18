dd4ce6210b86f83c7bf2cdd1c6ac1b65e60c2f72
teffidev <tefyberal09@gmail.com>
2026-02-18 15:16:52 -0500

    Simple Payroll

---
+++ LogicConcepts.2026.1/SimplePayroll/Program.cs
@@ -0,0 +1,33 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+do
+{
+    var name = ConsoleExtensions.GetString("Ingrese nombre..........................: ");
+    var hoursWorked = ConsoleExtensions.GetFloat("Ingrese número de horas trabajadas......: ");
+    var hourValue = ConsoleExtensions.GetDecimal("Ingrese valor hora......................: ");
+    var salaryMinimum = ConsoleExtensions.GetDecimal("Ingrese valor salario mínimo mensual....: ");
+
+    // calculate the salary of the employee
+    var salary = (decimal)hoursWorked * hourValue;
+
+    if (salary < salaryMinimum)
+    {
+        Console.WriteLine($"Nombre..................................: {name}");
+        //Console.WriteLine($"Salario.................................: {salaryMinimum:C2}");
+    }
+    else
+    {
+        Console.WriteLine($"Nombre..................................: {name}");
+        Console.WriteLine($"Salario.................................: {salary:C2}");
+    }
+    
+    // Validate that the user enters a valid option to continue or not with the programme
+    do
+    {
+        answer = ConsoleExtensions.GetValidOptions("¿Desea continuar [S]i, [N]o}?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
