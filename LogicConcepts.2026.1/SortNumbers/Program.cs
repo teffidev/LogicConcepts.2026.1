07b0c69c734aac1715e152af5bcaba57480c6f35
teffidev <tefyberal09@gmail.com>
2026-02-18 13:24:00 -0500

    Sort numbers

---
+++ LogicConcepts.2026.1/SortNumbers/Program.cs
@@ -0,0 +1,57 @@
+﻿using Shared;
+
+do
+{
+    Console.WriteLine("Ingrese tres números diferentes.");
+    var a = ConsoleExtensions.GetInt("Ingrese primer número : ");
+    var b = ConsoleExtensions.GetInt("Ingrese segundo número : ");
+
+    if (a == b)
+    {
+        Console.WriteLine("Los numeros deben ser diferentes, vuelva a empezar");
+        continue;
+    }
+
+    var c = ConsoleExtensions.GetInt("Ingrese tercer número : ");
+
+    if (b == c || c == a)
+    {
+        Console.WriteLine("Los numeros deben ser diferentes, vuelva a empezar");
+        continue;
+    }
+
+    if (a > b && a > c)
+    {
+        if (b > c)
+        {
+            Console.WriteLine($"El mayor es {a}, el medio es {b}, el menor es {c}");
+        }
+        else
+        {
+            Console.WriteLine($"El mayor es {a}, el medio es {c}, el menor es {b}");
+        }
+    }
+    else if (b > a && b > c)
+    {
+        if (a > c)
+        {
+            Console.WriteLine($"El mayor es {b}, el medio es {a}, el menor es {c}");
+        }
+        else
+        {
+            Console.WriteLine($"El mayor es {b}, el medio es {c}, el menor es {a}");
+        }
+    }
+    else
+    {
+        if (a > b)
+        {
+            Console.WriteLine($"El mayor es {c}, el medio es {a}, el menor es {b}");
+        }
+        else
+        {
+            Console.WriteLine($"El mayor es {c}, el medio es {b}, el menor es {a}");
+        }
+    }
+
+} while (true);
